8ffa7480262825ee12f0f5756ce1d9f77c32f533
Michelle-Lefebvre <mmldesigns007@gmail.com>
2020-11-11 19:56:14 -0500

    get all movies and display by category rows

--- src/Row.js
+++ src/Row.js
@@ -1,24 +1,41 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from './axios';
+import './row.css'
 
-function Row({ title }) {
+const base_url = 'https://image.tmdb.org/t/p/original/';
+
+function Row({ title, fetchUrl }) {
     const [movies, setMovies] = useState([]);
+
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(fetchUrl);
-            console.log(request);
+            // console.log(request.data.results);
+            setMovies(request.data.results);
             return request;
         }
-        fetchData()
-    }, []);
+        fetchData();
+    }, [fetchUrl]);
+
+    console.log(movies);
 
     return (
-        <div>
+        <div className="row">
             <h2>{title}</h2>
+            <div className="row__posters">
+                {movies.map(movie => (
+                    <img
+                        key={movie.id}
+                        className="row__poster"
+                        src={`${base_url}${movie.poster_path}`}
+                        alt={movie.name} />
+                ))}
+            </div>
 
-        </div>
+
+        </div >
     )
 }
 
